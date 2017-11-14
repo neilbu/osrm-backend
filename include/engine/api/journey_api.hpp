24c78b910269c3e14231ebf919b7861db334efe3
Neil Buckman <neil.buckman@chopchops.co.uk>
2017-11-14 10:44:50 +0000

    Put the coordinates in with the journey info

--- include/engine/api/journey_api.hpp
+++ include/engine/api/journey_api.hpp
@@ -40,47 +40,29 @@ class JourneyAPI final : public BaseAPI
                               const std::vector<PhantomNode> &phantoms,
                               util::json::Object &response) const
     {
-        auto number_of_coordinates = phantoms.size() / 2;
-        SetCoordinates(phantoms, number_of_coordinates, response);
-        response.values["durations"] = MakeResultTable(durations);
+        response.values["journeys"] = MakeResultTable(phantoms, durations);
         response.values["code"] = "Ok";
     }
 
   protected:
-    virtual void SetCoordinates(const std::vector<PhantomNode> &phantoms,
-                                std::size_t num_coordinates,
-                                util::json::Object &response) const
-    {
-        util::json::Array json_sources;
-        json_sources.values.reserve(num_coordinates);
-        util::json::Array json_targets;
-        json_targets.values.reserve(num_coordinates);
-        BOOST_ASSERT(phantoms.size() == parameters.coordinates.size());
-        
-        for (unsigned sourceIndex = 0; sourceIndex < num_coordinates; sourceIndex++)
-        {
-            const auto sourceNode = phantoms[sourceIndex * 2];
-            util::json::Array sourceArray;
-            sourceArray.values.push_back(static_cast<double>(util::toFloating(sourceNode.location.lon)));
-            sourceArray.values.push_back(static_cast<double>(util::toFloating(sourceNode.location.lat)));
-            json_sources.values.push_back(sourceArray);
-            const auto targetNode = phantoms[(sourceIndex * 2) + 1];
-            util::json::Array targetArray;
-            targetArray.values.push_back(static_cast<double>(util::toFloating(targetNode.location.lon)));
-            targetArray.values.push_back(static_cast<double>(util::toFloating(targetNode.location.lat)));
-            json_targets.values.push_back(targetArray);
-        }
-        response.values["sources"] = json_sources;
-        response.values["targets"] = json_targets;
-    }
-
-    virtual util::json::Array MakeResultTable(const std::vector<std::pair<EdgeWeight, double>> &values) const
+    virtual util::json::Array MakeResultTable(const std::vector<PhantomNode> &phantoms,
+                                              const std::vector<std::pair<EdgeWeight, double>> &values) const
     {
         util::json::Array json_table;
         auto number_of_results = values.size();
         for (unsigned resultIndex = 0; resultIndex < number_of_results; ++resultIndex)
         {
             util::json::Object result = util::json::Object();
+            const auto sourceNode = phantoms[resultIndex * 2];
+            util::json::Array sourceArray;
+            sourceArray.values.push_back(static_cast<double>(util::toFloating(sourceNode.location.lon)));
+            sourceArray.values.push_back(static_cast<double>(util::toFloating(sourceNode.location.lat)));
+            result.values["from"] = sourceArray;
+            const auto targetNode = phantoms[(resultIndex * 2) + 1];
+            util::json::Array targetArray;
+            targetArray.values.push_back(static_cast<double>(util::toFloating(targetNode.location.lon)));
+            targetArray.values.push_back(static_cast<double>(util::toFloating(targetNode.location.lat)));
+            result.values["to"] = targetArray;
             if (values[resultIndex].first == MAXIMAL_EDGE_DURATION)
             {
                 result.values["distance"] = util::json::Null();
